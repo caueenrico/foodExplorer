e541e989416994d80a129516826c5a5b6683ea0e
caueenrico <caueenrico@gmail.com>
2024-03-31 02:23:57 -0300

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'foodfastapj',
+        short_name: 'fda',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+        icons: [
+          {
+            "src": "/public/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/public/manifest-icon-192.maskable.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          }
+        ],
+      },
+    }),
+  ],
+});
