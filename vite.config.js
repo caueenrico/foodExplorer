296a65e74127f37c35005a1cd2c1dd6e15b9ec73
caueenrico <caueenrico@gmail.com>
2024-04-01 15:10:00 -0300

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,42 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      registerType: "prompt",
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "android-chrome-192x192.png",
+      ],
       manifest: {
-        name: 'foodfastapj',
-        short_name: 'fda',
-        start_url: '/',
-        display: 'standalone',
-        theme_color: '#000000',
-        background_color: '#ffffff',
+        name: "foodfastapj",
+        short_name: "fda",
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff",
         icons: [
           {
-            "src": "/public/icon-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "maskable"
+            src: "./android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            "src": "/public/manifest-icon-192.maskable.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "any"
+            src: "./android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src:'/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'apple touch icon'
           }
         ],
       },
